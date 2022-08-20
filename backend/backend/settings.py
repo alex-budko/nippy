7e5469c0eca42bc1f56d96ac364c3a9c0cc687d4
alex-budko <xalex.budko@gmail.com>
2022-08-20 00:00:06 -0400

    deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
